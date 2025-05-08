1b3693aeccf2528ce343d2da5825bf6ca25cb8ea
Derian Rojas <116598387+derianbv@users.noreply.github.com>
2025-05-08 12:12:02 -0500

    Update 8_mayo.c

--- 8_mayo.c
+++ 8_mayo.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 int main() {
-    float f = 3.14f;        // The float value
+    float f = -3.14f;        // The float value
     uint32_t hexValue; /* 
 Esta parte del codigo es posible gracias 
 
@@ -17,7 +17,8 @@ Esta parte del codigo es posible gracias
     // Print the float and its hex representation, lo que se ve en la clase, de que 4 bytes pueden ser varios numeros depende de su representación. 
     printf("Float: %f\n", f); // formato IEEE decimal 
     printf("Hex: 0x%08X\n", hexValue); //entero con signo     
-    printf("Dec:" %d\n", hexValue); //entero sin signo
+    printf("Dec:" %ud\n", hexValue); //entero sin signo
+    printf(Dec SIGN: %d\n", hexValue); 
 
     return 0;
 }

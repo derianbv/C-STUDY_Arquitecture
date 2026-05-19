0e3a35f0c185f298422bd8b743304edf26a65878
Derian Rojas <116598387+derianbv@users.noreply.github.com>
2026-05-19 16:42:25 -0500

    Update Servidor.c

--- Parcial2/pasoDeMensajes/Servidor.c
+++ Parcial2/pasoDeMensajes/Servidor.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/socket.h> //socket 
+#include <netinet/in.h> //sockaddr_in, htons()
+#include <arpa/inet.h>
+#include <strings.h>
+#include <unistd.h>
+
 

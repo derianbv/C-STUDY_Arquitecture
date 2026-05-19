48c061cb1dd67d3f13641b2ba51969dc095abcb5
Derian Rojas <116598387+derianbv@users.noreply.github.com>
2026-05-19 17:07:49 -0500

    Update Servidor.c

--- Parcial2/pasoDeMensajes/Servidor.c
+++ Parcial2/pasoDeMensajes/Servidor.c
@@ -7,4 +7,22 @@
 #include <strings.h>
 #include <unistd.h>
 
+//Comunicación por red. 
 
+
+#define PORT 3535
+#define BACKLOG 8
+
+//1. ------------- se crean los enteros 
+int sockfd; 
+int sockfdc1; 
+int r; 
+
+//2. se crean las estructuras que van a guardar las direcciones IP de servidor y Cliente. 
+struct sockaddr_in servidor;
+struct sockaddr_in cliente; 
+
+//3. socketlen_t // crear el largo de los sockets  (podría ser Int pero por portabilidad es socketlen_t) 
+socklen_t addresLenServidor;
+socklen addressLenCliente; 
+double pi; 
